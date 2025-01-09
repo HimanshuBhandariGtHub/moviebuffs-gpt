80a170d266e848464e67ec2c6dedac9960cbaf56
HimanshuBhandariGtHub <himanshubhandari2023.com>
2025-01-10 01:32:07 +0530

    added validation to sign in/sign up form

---
+++ src/assets/utils/validate.jsx
@@ -0,0 +1,14 @@
+export const checkValidateData = (email, password, fullName) =>{
+    
+    const isFullNameValid =/(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(fullName);
+    const isEmailValid = /^[a-zA-Z0-9_.+\-]+[\x40][a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/.test(email); 
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}/.test(password);
+
+    if(!isFullNameValid) return "Name not valid";
+    if(!isEmailValid) return "Email Id not valid";
+    if(!isPasswordValid) return "Password not valid";
+    
+
+    return null; 
+
+}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Header from './Header'
+import { checkValidateData } from '../assets/utils/validate';
 
 const Login = () => {
   const[isSignInForm, setIsSignInForm] = useState(true);
+  const[errorMessage, setErrorMessage] = useState(null);
+
+  const fullName = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick=()=>{
+    //validate the form data
+    console.log(fullName);
+    const message= checkValidateData(email.current.value, password.current.value, fullName.current.value);
+    setErrorMessage(message);
+
+    // sign in/sign up
+  }
 
   const toggleSignInForm =()=>{
     setIsSignInForm(!isSignInForm);
@@ -13,12 +28,13 @@ const Login = () => {
       <div className="absolute">
       <img src="https://assets.nflxext.com/ffe/siteui/vlv3/2f5a878d-bbce-451b-836a-398227a34fbf/web/IN-en-20241230-TRIFECTA-perspective_5ab944a5-1a71-4f6d-b341-8699d0491edd_large.jpg" alt="bg-image"/>
       </div>
-      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form onSubmit={(e) => e.preventDefault()} className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
         <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-        {!isSignInForm && <input className="p-4 my-4 w-full bg-gray-700" type="text" placeholder="Full Name" />}
-        <input className="p-4 my-4 w-full bg-gray-700" type="text" placeholder="Email Address" />
-        <input className="p-4 my-4 w-full bg-gray-700" type="password" placeholder="Password" />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        {!isSignInForm && <input ref={fullName} className="p-4 my-4 w-full bg-gray-700" type="text" placeholder="Full Name" />}
+        <input ref={email} className="p-4 my-4 w-full bg-gray-700" type="text" placeholder="Email Address" />
+        <input ref={password} className="p-4 my-4 w-full bg-gray-700" type="password" placeholder="Password" />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up now" : "Already a User? Sign In now"}</p>
       </form>
     </div>
